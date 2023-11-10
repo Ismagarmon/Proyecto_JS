4f3bf25a0da54b4a1807f92f10e4b0969afa87b9
Ismael <ismagarmon3@gmail.com>
2023-11-10 12:42:14 +0100

    update

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,14 @@
-import { Musica, TOGGLE } from "./module.js";
+import { TOGGLE, getCardList, getMusicList } from "./module.js";
 
 window.onload = () => {
     pprincipal()
     loadmusicplayer()
+    Controls()
+
+    getMusicList().then((data) => {
+        //let cant = data.Music.length
+        console.log(data)
+    }).catch((error) => alert(`El error es: ${error}`))
 }
 
 let nombre = null;
@@ -13,14 +19,13 @@ const pprincipal = () => {
     let div_p = document.getElementById('home')
     let btn = document.getElementById('btn-principal')
     let div_principal = document.getElementById('p-principal')
-    
-    btn.addEventListener('click',() => {
+
+    btn.addEventListener('click', () => {
         nombre = input.value
         div_p.classList.add('none')
         div_principal.classList.remove('none')
         document.cookie = `Nombre=${nombre}`
         input.value = ''
-        console.log(nombre)
     })
 }
 
@@ -30,8 +35,68 @@ const loadmusicplayer = () => {
 
     let button_mp = document.getElementById('reproductor')
 
-    button_mp.addEventListener('click',() => {
+    button_mp.addEventListener('click', () => {
         div_p.classList.add('none')
         div.classList.remove('none')
     })
+}
+
+function Controls() {
+    const reproductor = new Reproductor()
+    let svg_play = document.getElementById('play')
+    let svg_pause = document.getElementById('pause')
+    let svg_atras = document.getElementById('atras')
+    let svg_adelante = document.getElementById('adelante')
+
+    svg_play.addEventListener('click', () => {
+        reproductor.play()
+        svg_play.classList.add('none')
+        svg_pause.classList.remove('none')
+    })
+    svg_pause.addEventListener('click', () => {
+        reproductor.pause()
+        svg_play.classList.remove('none')
+        svg_pause.classList.add('none')
+    })
+    svg_atras.addEventListener('click', () => {
+        reproductor.otracancion()
+    })
+    svg_adelante.addEventListener('click', () => {
+        reproductor.otracancion()
+    })
+}
+
+class Reproductor {
+
+    constructor() {
+        this.p_titulo = document.getElementById('titulo_song')
+        this.img = document.getElementById('song_image')
+        this.p_titulo.textContent = "Initial D - Spitfire"
+        this.img.src = "https://vmndims.binge.com.au/api/v2/img/5e704b06e4b0f4391761e2d6-1584417689045?location=tile&imwidth=1280"
+        this.audio = new Audio('audio/ID_Spitfire.mp3')
+        this.audio.volume = 0.6
+    }
+
+    play() {
+        this.audio.play()
+    }
+
+    pause() {
+        this.audio.pause()
+    }
+
+    otracancion() {
+        getMusicList()
+        .then((data) => {
+            let cant = data.Music.length
+            let object = data.Music
+            console.log(cant)
+            let v = Math.floor(Math.random() * cant);
+            this.p_titulo.textContent = object[v].Titulo
+            this.img.src = object[v].src_img
+            this.audio.src = object[v].src_audio
+        })
+        .catch((error) => alert(`El error es: ${error}`))
+    }
+    
 }
--- scripts/module.js
+++ scripts/module.js
@@ -1,36 +1,21 @@
-class Musica {
-
-    Id = '';
-    Url = '';
-    Nombre = '';
-
-    constructor(id,url,nombre) {
-        this.Id = id
-        this.Url = url
-        this.Nombre = nombre
-    }
-
-    getName() {
-        return this.Nombre
-    }
-
-    getId() {
-        return this.Id
-    }
-
-    getUrl() {
-        return this.Url
-    }
-
-
-}
-
-export { Musica }
-
 export const TOGGLE = {
-    KEYUP: 'keyup',
-    KEYDOWN: 'keydown',
-    KEYRIGHT: 'keyright',
-    KEYLEFT: 'keyleft'
+    UP: 'keyup',
+    DOWN: 'keydown',
+    RIGHT: 'keyright',
+    LEFT: 'keyleft',
+    ENTER: 'Enter'
 }
 
+export async function getMusicList() {
+    const loadjson = await fetch('../json/MusicList.json')
+    const parsejson = await loadjson.json()
+
+    return parsejson
+}
+
+export async function getCardList() {
+    const loadjson = await fetch('../json/CardList.json')
+    const parsejson = await loadjson.json()
+
+    return parsejson
+}
